f52af7808d0aceb7b642dc17caaf724befb63a23
Mohammad Aqib Hassan <h_aqib11@rocketmail.com>
2022-01-12 21:10:14 +0200

    Get /run-log returns a 200 when called passed

--- message_queue/service_api/test/messagequeuetest.test.js
+++ message_queue/service_api/test/messagequeuetest.test.js
@@ -49,6 +49,17 @@ describe("Testing the API", function () {
       });
   })
 
-  
+  it("Get /run-log returns a 200 when called", function (done) {
+    nock("http://localhost:8083")
+      .get('/run-log')
+      .reply(200, "2020-11-19T18:05:36.037Z: INIT");
+    request
+      .get('/run-log')
+      .end(function (err, res) {
+        expect(res.status).to.equal(200);
+        expect(res.text).not.to.be.null;
+        done();
+      });
+  })
 });
 
